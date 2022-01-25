d982e136c803b800eb86b36d7e8a9bb7a4f32e65
Shmee <smaheilman@gmail.com>
2022-01-24 20:48:05 -0500

    functional

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-const path = require('path');
+//npm i express
 const express = require('express');
+const routes = require('./controllers');
+//npm i sequelize
+const sequelize = require('./config/connection');
+// npm install --save path
+const path = require('path');
+// npm i express-session connect-session-sequelize
 const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+//MIGHT USE THESE LATER
+// // npm i handlebars, npm i express-handlebars
+// const exphbs = require('express-handlebars');
+// //imports handlebar helpers
+// const helpers = require('./utils/helpers');
+// const hbs = exphbs.create({ helpers });
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'Super duper duper secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -19,21 +29,22 @@ const sess = {
   })
 };
 
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({helpers});
+
 app.use(session(sess));
-
-const helpers = require('./utils/helpers');
-
-const hbs = exphbs.create({ helpers });
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(routes);
 
+// turn on connection to db and server
+// force true will always sync to server (reset) upon npm start
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
